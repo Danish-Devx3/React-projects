638babe4f7ca4564fcdacae731e0e9957fcbd1c8
Danish-Devx3 <danish.devx3@gmail.com>
2025-01-18 23:44:36 +0530

    update button border color in StartGame component for improved styling

--- Dice-Game/src/components/StartGame.jsx
+++ Dice-Game/src/components/StartGame.jsx
@@ -10,7 +10,7 @@ const StartGame = ({toggle}) => {
       <div>
         <h1 className='text-8xl font-semibold text-nowrap'>Dice Game</h1>
         <button onClick={toggle} className='text-white px-[18px] py-[10px] rounded-md bg-black min-w-[220px] text-base transition ease-in-out delay-150 bg-black
-        border-2 border-transparent hover:bg-white hover:border-2 border-slate-900 hover:text-black '>Play now</button>
+        border-2 border-transparent hover:bg-white hover:border-2 border-zinc-950 hover:text-black '>Play now</button>
       </div>
     </section>
     </div>
