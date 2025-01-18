297794ef3cfcc138413dff3b34116098c6f0c71a
Danish-Devx3 <danish.devx3@gmail.com>
2025-01-18 23:02:20 +0530

    update initial game state to false in App component

--- Dice-Game/src/App.jsx
+++ Dice-Game/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import StartGame from './components/StartGame'
 import Game from './components/Game'
 
 function App() {
-  const [startGame, setStartGame] = useState(true)
+  const [startGame, setStartGame] = useState(false)
 
   const toggle = () =>{
     setStartGame((prev)=>!prev)
