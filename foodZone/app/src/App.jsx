6472b27044456facc3dd7d4f3867a397a9d8a399
Danish-Devx3 <danish.devx3@gmail.com>
2025-03-20 16:58:40 +0530

    Add initial setup for foodZone app with images, configuration files, and basic structure

---
+++ foodZone/app/src/App.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const App = () => {
+  return <>
+    <MainContainer>
+        <TopContainer>
+          <div className="logo">
+            <img src="/Foody Zone.svg" alt="" />
+          </div>
+
+          <div className="search">
+            <input type="text" placeholder="Search Food" />
+          </div>
+        </TopContainer>
+    </MainContainer>
+
+  </>;
+};
+
+export default App;
+
+const MainContainer = styled.div``;
+const TopContainer = styled.section``;
